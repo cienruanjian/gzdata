815556dc20e408216d10a962e2ed831782e938f4
yuwei <727955957@qq.com>
2015-10-21 14:51:29 +0800

    commit

--- Application/Home/Controller/LoginController.class.php
+++ Application/Home/Controller/LoginController.class.php
@@ -5,7 +5,7 @@ namespace Home\Controller;
 /**
  * 用户登陆控制器
  * @author yuwei
- * @date 2015-07-04
+ * @date 2015-10-21
  */
 class LoginController extends BaseController
 {
@@ -13,24 +13,6 @@ class LoginController extends BaseController
 	//登陆页视图
 	public function index()
 	{
-
-		$role = I('role', 0, 'intval');
-		switch ($role) {
-			case 20:
-				$title = '民调测评平台';
-				break;
-			case 30:
-				$title = '督查情况测评平台';
-				break;
-			case 40:
-				$title = '日常监控测评平台';
-				break;
-			default:
-				$this->error('非法操作');
-				break;
-		}
-		$this->role = $role;
-		$this->title = $title;
 		$this->display();
 	}
 
@@ -82,29 +64,4 @@ class LoginController extends BaseController
             $this->redirect('Index/index');
         }
     }
-
-    /**
-	 *  异步发送验证码
-	 * @return [type] [description]
-	 */
-	public function verify() {
-        if (!IS_AJAX) $this->error('非法操作');
-        $login = I('login');
-        $role = I('role');
-        if (!$login) {
-                echo '请输入用户名...';
-                exit;
-        }
-        $model = M('User');
-        $phone = $model->where(['login' => $login, 'role' => $role, 'site_id' => $this->_siteInfo['id']])->getField('phone');
-        if (!$phone) {
-                echo '用户或者号码不存在...';
-                exit;
-        }
-        import("Home.Util.Util");
-        $num = rand(1000, 9999);
-        $result = \Util::sendSMS ($phone, '您的验证码是：'.$num.'。请不要把验证码泄露给其他人。' );
-        session('verify', $num);
-        echo $result ['SubmitResult'] ['msg'];
-	}
 }
