c42c068c99ad5a477c967ea1936d1a15b809f8bd
yuwei <727955957@qq.com>
2015-10-21 14:48:57 +0800

    commit

---
+++ Application/Runtime/Cache/Home/05735e02ebe6fa2a905c27ba771a58b0.php
@@ -0,0 +1,143 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>云上贵州-登录</title>
+  <link href="css/head.css" rel="stylesheet" type="text/css" />
+  <link href="/Public/Home/css/jianjie.css" rel="stylesheet" type="text/css" />
+<!-- page common css here -->
+<link href="/Public/Home/Css/base.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<!--头部  开始-->
+<div id="head">
+  <div id="head1">
+    <div id="mav">
+          <div id="logo"><img src="/Public/Home/Images/logo.jpg" width="502" height="124" /></div>
+          <div id="neww11">
+              <div id="iymm1">
+                <input type="text" placeholder="请输入关键字搜索" / class="udq1">
+                <button type="submit" class="buttons"> </button>
+              </div>
+              <div class="iymm">
+                <p><a href="<?php echo U('Regist/index');?>">注册</a></p>
+                <p><a href="<?php echo U('Login/index');?>">登录</a></p>
+              </div>
+          </div>
+          <div id="neww">
+              <ul>
+                  <li><a href="<?php echo U('Index/index');?>">首页</a></li>
+                  <li><a href="<?php echo U('Intro/index');?>">大赛简介</a></li>
+                  <li><a href="<?php echo U('Article/index');?>">大赛资讯</a></li>
+                  <li><a href="<?php echo U('Enroll/index');?>">报名通道</a></li>
+                  <li><a href="<?php echo U('Help/index');?>">帮助中心</a></li>
+                  <li><a href="http://contest.gzdata.com.cn/" target="_blank">往届回顾 </a></li>
+              </ul>
+          </div>
+    </div>
+  <div class="clear"></div>
+</div>
+</div>
+  <div id="khuj" style="background:url(/Public/Home/images/jj.jpg) center top no-repeat;">
+    <div id="h302"></div>
+      <div class="all" style="over-flow:hidden">
+        <div class="all_1">
+          <div class="all_1_l">
+            <img src="/Public/Home/images/qzy/6.jpg" width="310" height="53" />
+          </div>
+          <div class="all_1_r">
+            <ul>
+              <li>评分细则</li>
+              <li>奖项设置</li>
+              <li>评审组</li>
+              <li>报名规则</li>
+              <li>大赛支持</li>
+              <li>大赛组织</li>
+              <li>赛制与流程</li>
+              <li>大赛简介</li>
+            </ul>
+          </div>
+          <div class="clear"></div>
+        </div>
+        <div class="all_2">
+          <div class="all_2_1">
+            <div class="all_2_1_l">大赛简介</div>
+            <div class="all_2_1_r">您现在的位置：首页>大赛简介</div>
+            <div class="clear"></div>
+          </div>
+          <div class="all_2_2">
+            <img src="/Public/Home/images/qzy/7.jpg" width="813" height="49" />
+          </div>
+          <div class="all_2_3">
+            <p>
+              贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业产业贵州在大数据产业
+            </p>
+            <p>
+              贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业产业贵州在大数据产业
+            </p>
+          </div>
+          <div class="all_2_2">
+            <img src="/Public/Home/images/qzy/8.jpg" width="812" height="49" />
+          </div>
+          <div class="all_2_4">2015年10月-2016年6月</div>
+          <div class="all_2_2">
+            <img src="/Public/Home/images/qzy/8.jpg" width="812" height="49" />
+          </div>
+          <div class="all_2_5">
+            <p>
+              贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产据产业贵州在大大数据产业贵州在大数据产业贵州在大数据产业贵州产业贵州在大数大数据产业
+            </p>
+            <p>
+              贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产据产业贵州在大大数据产业贵州在大数据产业贵州在大数据产业贵州产业贵州在大数大数据产业
+            </p>
+            <p>
+              贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产据产业贵州在大大数据产业贵州在大数据产业贵州在大数据产业贵州产业贵州在大数大数据产业
+            </p>
+            <p>
+              贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产据产业贵州在大大数据产业贵州在大数据产业贵州在大数据产业贵州产业贵州在大数大数据产业
+            </p>
+            <p>
+              贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产据产业贵州在大大数据产业贵州在大数据产业贵州在大数据产业贵州产业贵州在大数大数据产业
+            </p>
+            <p>
+              贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产业贵州在大数据产据产业贵州在大大数据产业贵州在大数据产业贵州在大数据产业贵州产业贵州在大数大数据产业
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="foot">
+  <div class="h50"></div>
+  <div id="foot1">
+      <div id="foot1_logo"><img src="/Public/Home/Images/logo1.jpg" width="156" height="115" /></div>
+      <div id="foot1_Title">
+         <h3><img src="/Public/Home/Images/m17.jpg" width="36" height="36" />　大赛组委会电话</h3>
+         <p class="mmq">0851-8626792</p>
+         <p>官方邮箱：gzdata@163.com</p>
+         <p>报名地址：contest.gzdata.com.cn</p>
+      </div>
+      <div id="foot_qq">
+         <p class="maz">QQ: 3086586463</p>
+         <p class="maz1">大数据商业模式大赛</p>
+      </div>
+      <div id="foot_rvtv">
+          <div id="foot_rvrv_img"><img src="/Public/Home/Images/m18.jpg" width="114" height="113" /></div>
+          <div id="foot_Tbr">
+              <p class="mn1">大数据商业模式大赛官方微信</p>
+              <p class="mn2">扫一扫，关注我们</p>
+              <p class="mn3">及时获取大数据大赛最新信息</p>
+              <p class="mn4">微信号：gzdatacontest</p>
+          </div>
+      </div>
+  </div>
+  <div id="foot_wqbu">
+    <p>黔ICP备15088888号  Copyrights @ 2014-2016 贵州省经济信息和信息化委员会 版权所有.All Rights Reservde    技术支持：慈恩软件</p>
+  </div>
+</div>
+<!-- common js plugin here -->
+<script type="text/javascript" src="/Public/Home/Js/jquery-1.8.3.min.js"></script>
+
+</body>
+</html>
