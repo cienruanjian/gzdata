a1da8b7c7cae975b2809586a9de6b39cc4155ef4
yuwei <727955957@qq.com>
2015-10-21 14:54:06 +0800

    commit

---
+++ Application/Common/Util/Util.class.php
@@ -0,0 +1,32 @@
+<?php
+
+class Util {
+	/**
+	 * 发送短信
+	 * Enter description here .
+	 * @param unknown_type $mobile        	
+	 * @param unknown_type $content        	
+	 */
+	static public function sendSMS($mobile, $content) {
+		import ( "Home.Util.Nusoap" );
+		
+		$client = new \nusoap_client ( 'http://106.ihuyi.com/webservice/sms.php?WSDL', 'wsdl' );
+		
+		$err = $client->getError ();
+		if ($err) {
+			echo '<h2>Constructor error</h2><pre>' . $err . '</pre>';
+		}
+		
+		$client->soap_defencoding = 'UTF-8';
+		$client->decode_utf8 = false;
+		$client->xml_encoding = 'UTF-8';
+		$parameters = array (
+				'account' => 'cf_dsjs',
+				'password' => 'd9-fiG.2F2J',
+				'content' => $content,
+				'mobile' => $mobile 
+		);
+		
+		return $client->call ( 'Submit', $parameters );
+	}
+}
